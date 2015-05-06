ca8882e77ef9029bc1c8d3afa06ca58e4d8ea579
Sam Coughlin <samjcoughlin@gmail.com>
2015-05-06 16:15:43 +1200

    Added all get_html api method options
    Added new overloaded methods to support passing through the additional
    api parameters: language and is_auto_submit

--- SweetCaptcha/SweetCaptcha.cs
+++ SweetCaptcha/SweetCaptcha.cs
@@ -11,6 +11,26 @@ namespace SweetCaptcha
         private readonly string _appKey;
         private readonly string _host;
 
+        private async Task<string> GetHtml(Dictionary<string, string> parameterDictionary)
+        {
+          var client = new HttpClient();
+          var request = await client.PostAsync(_host, new FormUrlEncodedContent(parameterDictionary));
+          return await request.Content.ReadAsStringAsync();
+        }
+
+        private Dictionary<string, string> DefaultParameters
+        {
+          get{
+            var parameters = new Dictionary<string, string>
+                {
+                    { "app_id", _appId },
+                    { "app_key", _appKey },
+                    { "method", "get_html" },
+                    { "platform", "sweetcaptcha-dotnet" },
+                };
+            return parameters;
+        }}
+
         public SweetCaptcha(string host, string appId, string appKey)
         {
             if (string.IsNullOrEmpty(host))
@@ -27,16 +47,20 @@ namespace SweetCaptcha
 
         public async Task<string> GetHtml()
         {
-            var client = new HttpClient();
-            var parameters = new Dictionary<string, string>
-            {
-                { "app_id", _appId },
-                { "app_key", _appKey },
-                { "method", "get_html" },
-                { "platform", "sweetcaptcha-dotnet" },
-            };
-            var request = await client.PostAsync(_host, new FormUrlEncodedContent(parameters));
-            return await request.Content.ReadAsStringAsync();
+          return await GetHtml(DefaultParameters);
+        }
+
+        public async Task<string> GetHtml(string language, bool isAutoSubmit = false)
+        {
+          var parameters = DefaultParameters;
+          parameters.Add("language", language);
+
+          if (isAutoSubmit)
+          {
+            parameters.Add("is_auto_submit", "1");
+          }
+
+          return await GetHtml(parameters);
         }
 
         public async Task<bool> Check(string sckey, string scvalue)
